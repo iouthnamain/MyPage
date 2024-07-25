75a3bd48842f968d95660b3c216c8c76e46e15f5
iouthnamain <iouthnamain@gmail.com>
2024-07-26 00:39:30 +0700

    added Info, Portforlio, Contact page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    // Add reveal animation for each header letter
     document.querySelectorAll('.reveal').forEach((element, index) => {
         element.style.animationDelay = `${index * 0.1}s`;
     });
 });
+
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
+    document.getElementById(pageId).style.display = 'block';
+}
